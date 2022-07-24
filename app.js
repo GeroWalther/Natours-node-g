280e93990b48e945210c0d20d1d10ef2607b5654
GeroWalther <gero.walther@gmail.com>
2022-07-24 19:03:33 +0200

    stuck with 500 error code!!

--- app.js
+++ app.js
@@ -1,72 +1,72 @@
-// // this file is used to configure express
-// const express = require('express');
-// const morgan = require('morgan');
-// const res = require('express/lib/response');
-// const { nextTick } = require('process');
-
-// const AppError = require('./utils/appError');
-// const globalErrorHandler = require('./controllers/errorController');
-// const tourRouter = require('./routes/tourRoutes');
-// const userRouter = require('./routes/userRoutes');
-
-// const app = express(); // adds a bunch of methods to the app variable such as listen
-
-// ////// 1) Middlewares - that should apply to all routes
-// if (process.env.NODE_ENV === 'developement') {
-//   app.use(morgan('dev'));
-// }
-// app.use(express.json());
-// app.use(express.static(`${__dirname}/public/`));
-
-// app.use((req, res, next) => {
-//   req.requestTime = new Date().toISOString();
-//   next();
-// });
-
-// //// 3) ROUTES - that should apply only to certain routes
-
-// app.use('/api/v1/users', userRouter);
-// app.use('/api/v1/tours', tourRouter); // specifies on which route we use tourRouter to create a sub-app for the resources - mounting a new router on a route; Has to come after we declare the variable - can not use router before declaring them.
-
-// app.all('*', (req, res, next) => {
-//   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
-// });
-
-// app.use(globalErrorHandler);
-
-// module.exports = app;
-
+// this file is used to configure express
 const express = require('express');
 const morgan = require('morgan');
+const res = require('express/lib/response');
+const { nextTick } = require('process');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-const app = express();
+const app = express(); // adds a bunch of methods to the app variable such as listen
 
-// 1) MIDDLEWARES
-if (process.env.NODE_ENV === 'development') {
+////// 1) Middlewares - that should apply to all routes
+if (process.env.NODE_ENV === 'developement') {
   app.use(morgan('dev'));
 }
-
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public/`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// 3) ROUTES
-app.use('/api/v1/tours', tourRouter);
+//// 3) ROUTES - that should apply only to certain routes
+
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter); // specifies on which route we use tourRouter to create a sub-app for the resources - mounting a new router on a route; Has to come after we declare the variable - can not use router before declaring them.
 
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 app.use(globalErrorHandler);
 
 module.exports = app;
+
+// const express = require('express');
+// const morgan = require('morgan');
+
+// const AppError = require('./utils/appError');
+// const globalErrorHandler = require('./controllers/errorController');
+// const tourRouter = require('./routes/tourRoutes');
+// const userRouter = require('./routes/userRoutes');
+
+// const app = express();
+
+// // 1) MIDDLEWARES
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(morgan('dev'));
+// }
+
+// app.use(express.json());
+// app.use(express.static(`${__dirname}/public`));
+
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
+
+// // 3) ROUTES
+// app.use('/api/v1/tours', tourRouter);
+// app.use('/api/v1/users', userRouter);
+
+// app.all('*', (req, res, next) => {
+//   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+// });
+
+// app.use(globalErrorHandler);
+
+// module.exports = app;
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -16,7 +16,7 @@ const sendErrorDev = (err, res) => {
 
 const sendErrorProd = (err, res) => {
   // Operational, trusted error: send message to client
-
+  console.log(err.isOperational);
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
@@ -42,7 +42,7 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'developement') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
